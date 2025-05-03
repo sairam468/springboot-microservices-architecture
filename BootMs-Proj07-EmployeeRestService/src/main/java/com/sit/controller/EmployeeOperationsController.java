9fb68b825d745fff5232b6174dda2da71204f316
GANTA LAKSHMAN NAGA DURGA SAIRAM <Sairamganta1@gmail.com>
2025-05-03 16:07:54 +0530

    Add files via upload

---
+++ BootMs-Proj07-EmployeeRestService/src/main/java/com/sit/controller/EmployeeOperationsController.java
@@ -0,0 +1,22 @@
+package com.sit.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeOperationsController {
+
+	@Value("${dbuser}")
+	private String user;
+	
+	@Value("${dbpwd}")
+	private String pw;
+	
+	@GetMapping("/show")
+	public String showData() {
+		return "Employee Data Collected Through Config server :: User "+user+"............. PassWord "+pw;
+	}
+}
